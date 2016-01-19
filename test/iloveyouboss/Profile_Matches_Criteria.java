d8538d6e43c24d99713c49a38c6dec335a9aedc3
Hao Hu <hhu@thoughtworks.com>
2016-01-19 16:41:02 +0800

    move single criterion tests to Profile_Matches_Criterion class

--- test/iloveyouboss/Profile_Matches_Criteria.java
+++ test/iloveyouboss/Profile_Matches_Criteria.java
@@ -9,95 +9,20 @@ import static org.junit.Assert.*;
 public class Profile_Matches_Criteria {
 
     private Profile profile;
-    private Criteria criteria;
-    private Question question;
 
     @Before
     public void setUp() {
-
         profile = new Profile("Test Profile");
-        criteria = new Criteria();
-        question = new BooleanQuestion(1, "Test Question");
-
-        Answer profileAnswer = new Answer(question, Bool.FALSE);
-        profile.add(profileAnswer);
     }
 
     @Test
     public void false_when_empty() {
-        assertThat(profile.matches(criteria), is(false));
+        assertThat(profile.matches(new Criteria()), is(false));
     }
 
     @Test
     public void score_is_0_when_empty() {
-        profile.matches(criteria);
-        assertThat(profile.score(), is(0));
-    }
-
-    @Test
-    public void true_when_has_single_and_dont_care() {
-
-        Answer criterionAnswer = new Answer(question, Bool.TRUE);
-        Criterion criterion = new Criterion(criterionAnswer, Weight.DontCare);
-        criteria.add(criterion);
-
-        assertThat(profile.matches(criteria), is(true));
-    }
-
-    @Test
-    public void score_is_weight_value_when_has_single_and_dont_care() {
-
-        Answer criterionAnswer = new Answer(question, Bool.TRUE);
-        Criterion criterion = new Criterion(criterionAnswer, Weight.DontCare);
-        criteria.add(criterion);
-
-        profile.matches(criteria);
-
-        assertThat(profile.score(), is(Weight.DontCare.getValue()));
-    }
-
-
-    @Test
-    public void true_when_has_single_and_match() {
-
-        Answer criterionAnswer = new Answer(question, Bool.FALSE);
-        Criterion criterion = new Criterion(criterionAnswer, Weight.Important);
-        criteria.add(criterion);
-
-        assertThat(profile.matches(criteria), is(true));
-    }
-
-    @Test
-    public void score_is_weight_value_when_has_single_and_match() {
-
-        Answer criterionAnswer = new Answer(question, Bool.FALSE);
-        Criterion criterion = new Criterion(criterionAnswer, Weight.Important);
-        criteria.add(criterion);
-
-        profile.matches(criteria);
-
-        assertThat(profile.score(), is(Weight.Important.getValue()));
-    }
-
-    @Test
-    public void false_when_has_single_and_not_match() {
-
-        Answer criterionAnswer = new Answer(question, Bool.TRUE);
-        Criterion criterion = new Criterion(criterionAnswer, Weight.Important);
-        criteria.add(criterion);
-
-        assertThat(profile.matches(criteria), is(false));
-    }
-
-    @Test
-    public void score_is_0_when_has_single_and_not_match() {
-
-        Answer criterionAnswer = new Answer(question, Bool.TRUE);
-        Criterion criterion = new Criterion(criterionAnswer, Weight.Important);
-        criteria.add(criterion);
-
-        profile.matches(criteria);
-
+        profile.matches(new Criteria());
         assertThat(profile.score(), is(0));
     }
 }
