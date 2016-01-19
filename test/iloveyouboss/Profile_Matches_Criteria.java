5db145c3b265a1318f1991a62ae7f673abe7017a
Hao Hu <hhu@thoughtworks.com>
2016-01-19 15:15:52 +0800

    move criteria, question to @Before block

--- test/iloveyouboss/Profile_Matches_Criteria.java
+++ test/iloveyouboss/Profile_Matches_Criteria.java
@@ -9,35 +9,37 @@ import static org.junit.Assert.*;
 public class Profile_Matches_Criteria {
 
     private Profile profile;
+    private Criteria criteria;
+    private Question question;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+
         profile = new Profile("Test Profile");
+        criteria = new Criteria();
+        question = new BooleanQuestion(1, "Test Question");
+
+        Answer profileAnswer = new Answer(question, Bool.FALSE);
+        profile.add(profileAnswer);
     }
 
     @Test
     public void false_when_empty() {
-        assertThat(profile.matches(new Criteria()), is(false));
+        assertThat(profile.matches(criteria), is(false));
     }
 
     @Test
     public void score_is_0_when_empty() {
-        profile.matches(new Criteria());
+        profile.matches(criteria);
         assertThat(profile.score(), is(0));
     }
 
     @Test
     public void true_when_has_single_criterion_and_dont_care() {
 
-        Criteria criteria = new Criteria();
-
-        Question question = new BooleanQuestion(1, "Test Question");
         Answer criterionAnswer = new Answer(question, Bool.TRUE);
         Criterion criterion = new Criterion(criterionAnswer, Weight.DontCare);
         criteria.add(criterion);
-
-        Answer profileAnswer = new Answer(question, Bool.FALSE);
-        profile.add(profileAnswer);
 
         assertThat(profile.matches(criteria), is(true));
     }
@@ -45,15 +47,9 @@ public class Profile_Matches_Criteria {
     @Test
     public void score_is_weight_value_when_has_single_criterion_and_dont_care() {
 
-        Criteria criteria = new Criteria();
-
-        Question question = new BooleanQuestion(1, "Test Question");
         Answer criterionAnswer = new Answer(question, Bool.TRUE);
         Criterion criterion = new Criterion(criterionAnswer, Weight.DontCare);
         criteria.add(criterion);
-
-        Answer profileAnswer = new Answer(question, Bool.FALSE);
-        profile.add(profileAnswer);
 
         profile.matches(criteria);
 
