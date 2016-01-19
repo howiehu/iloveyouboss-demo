b264cd663f7280188999aaeadd90de7c4f5f4988
Hao Hu <hhu@thoughtworks.com>
2016-01-19 15:01:20 +0800

    add 2nd behavior's tests

--- test/iloveyouboss/Profile_Matches_Criteria.java
+++ test/iloveyouboss/Profile_Matches_Criteria.java
@@ -25,4 +25,38 @@ public class Profile_Matches_Criteria {
         profile.matches(new Criteria());
         assertThat(profile.score(), is(0));
     }
+
+    @Test
+    public void true_when_has_single_criterion_and_dont_care() {
+
+        Criteria criteria = new Criteria();
+
+        Question question = new BooleanQuestion(1, "Test Question");
+        Answer criterionAnswer = new Answer(question, Bool.TRUE);
+        Criterion criterion = new Criterion(criterionAnswer, Weight.DontCare);
+        criteria.add(criterion);
+
+        Answer profileAnswer = new Answer(question, Bool.FALSE);
+        profile.add(profileAnswer);
+
+        assertThat(profile.matches(criteria), is(true));
+    }
+
+    @Test
+    public void score_is_weight_value_when_has_single_criterion_and_dont_care() {
+
+        Criteria criteria = new Criteria();
+
+        Question question = new BooleanQuestion(1, "Test Question");
+        Answer criterionAnswer = new Answer(question, Bool.TRUE);
+        Criterion criterion = new Criterion(criterionAnswer, Weight.DontCare);
+        criteria.add(criterion);
+
+        Answer profileAnswer = new Answer(question, Bool.FALSE);
+        profile.add(profileAnswer);
+
+        profile.matches(criteria);
+
+        assertThat(profile.score(), is(Weight.DontCare.getValue()));
+    }
 }
